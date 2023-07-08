0e39df61b1cfbdb8c283ee6fc6af5f37104073f5
Pedro Ribeiro <pedro.heribeiro6795@gmail.com>
2023-07-08 00:16:27 -0300

    create backend structure, Server running

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoModule } from './ToDo/toDo.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_DATABASE,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      synchronize: true,
+      entities: [__dirname + '/**/*.entity{.js,.ts}'],
+    }),
+    TodoModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
