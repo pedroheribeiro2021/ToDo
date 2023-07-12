05dfa31011318a4a54458d18caa60e10ea520204
Pedro Ribeiro <pedro.heribeiro6795@gmail.com>
2023-07-11 21:52:16 -0300

    create to-do CRUD logic  on frontend

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,30 +1,138 @@
 <script>
-	export let name;
-</script>
+	let todos = [];
+	let title = '';
+  	let noteContent = '';
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+	async function handleSubmit() {
+    const data = {
+      title,
+      noteContent
+    };
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    try {
+      const response = await fetch('http://localhost:3003/todos', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+	  if (!response.ok) {
+        throw new Error('Erro ao criar tarefa');
+      }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
+      const createdTodo = await response.json();
+      console.log('Tarefa criada:', createdTodo);
+      todos = [...todos, createdTodo];
+    } catch (error) {
+      console.error('Erro:', error.message);
+      // Trate o erro de acordo com a sua necessidade
+    }
+  }
+  
+	const fetchTodos = async () => {
+	  try {
+		const response = await fetch('http://localhost:3003/todos');
+		if (!response.ok) {
+		  throw new Error('Erro ao buscar as tarefas');
 		}
-	}
-</style>
+		todos = await response.json();
+	  } catch (error) {
+		console.error('Erro:', error.message);
+	  }
+	};
+  
+	const deleteTodo = async (id) => {
+	  try {
+		const response = await fetch(`http://localhost:3003/todos/${id}`, {
+		  method: 'DELETE',
+		});
+  
+		if (!response.ok) {
+		  throw new Error('Erro ao excluir tarefa');
+		}
+  
+		todos = todos.filter(todo => todo.id !== id);
+		console.log('Tarefa excluída:', id);
+	  } catch (error) {
+		console.error('Erro:', error.message);
+		// Trate o erro de acordo com a sua necessidade
+	  }
+	};
+
+	const updateNote = async (id, newTitle, newNoteContent) => {
+    try {
+      const data = {
+        title: newTitle,
+        noteContent: newNoteContent
+      };
+
+      const response = await fetch(`http://localhost:3003/todos/${id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+
+      if (!response.ok) {
+        throw new Error('Erro ao atualizar tarefa');
+      }
+
+      const updatedTodo = await response.json();
+      console.log('Tarefa atualizada:', updatedTodo);
+      todos = todos.map(todo => {
+        if (todo.id === updatedTodo.id) {
+          return updatedTodo;
+        }
+        return todo;
+      });
+    } catch (error) {
+      console.error('Erro:', error.message);
+      // Trate o erro de acordo com a sua necessidade
+    }
+  };
+
+	fetchTodos();
+
+  </script>
+  
+  <h1>Lista de Tarefas</h1>
+  
+  <!-- Formulário para criação de tarefas -->
+  <form on:submit|preventDefault={handleSubmit}>
+	<label>
+	  Título:
+	  <!-- <input type="text" name="title" required> -->
+	  <input type="text" bind:value={title} />
+	</label>
+	<label>
+	  Conteúdo da Tarefa:
+	  <textarea bind:value={noteContent}></textarea>
+	  <!-- <textarea name="noteContent" required></textarea> -->
+	</label>
+	<button type="submit">Criar Tarefa</button>
+  </form>
+
+  <!-- <ul>
+	{#each todos as todo}
+	  <li>
+		<h3>{todo.title}</h3>
+		<p>{todo.noteContent}</p>
+		<button on:click={() => deleteTodo(todo.id)}>Excluir</button>
+	  </li>
+	{/each}
+  </ul> -->
+<ul>
+  {#each todos as todo}
+    <li>
+      <h3>{todo.title}</h3>
+      <textarea bind:value={todo.title}></textarea>
+      <p>{todo.noteContent}</p>
+      <textarea bind:value={todo.noteContent}></textarea>
+      <button on:click={() => updateNote(todo.id, todo.title, todo.noteContent)}>Atualizar</button>
+      <button on:click={() => deleteTodo(todo.id)}>Excluir</button>
+    </li>
+  {/each}
+</ul>
